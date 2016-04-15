a988b8c616ad6c0f656201f7e30b83c66f7e24e1
Rahul Kadyan <hi@znck.me>
2016-04-15 10:34:23 +0530

    Missed a line

--- src/runner.py
+++ src/runner.py
@@ -136,6 +136,8 @@ def task_test_cnn(args):
         tmp = model.predict(x, args.mini_batch, args.verbose)
         out2 = np_append(out2, tmp)
     out = numpy.reshape(out[:, 0], dataset.image_size)
+    out1 = numpy.reshape(out1[:, 0], dataset.image_size)
+    out2 = numpy.reshape(out2[:, 0], dataset.image_size)
     numpy.save(change_ext(args.input, 'predicted.npy'), out)
     numpy.save(change_ext(args.input, 'model1.predicted.npy'), out1)
     numpy.save(change_ext(args.input, 'model2.predicted.npy'), out2)
